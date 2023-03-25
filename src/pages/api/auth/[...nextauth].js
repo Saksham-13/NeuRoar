ce555113b665f06261df862f7f3934fec89af454
saicharan1901 <papinenisaicharan@gmail.com>
2023-03-25 09:40:40 +0530

    update

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -4,8 +4,8 @@ import GitHubProvider from "next-auth/providers/github";
 const options = {
     providers: [
         GitHubProvider({
-            clientId: "01b5e3d55475c1ae680c",
-            clientSecret: "eef55906f2c61921d750fb8c7be3d9a58b0c6a88"
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
         }),
     ],
 }
